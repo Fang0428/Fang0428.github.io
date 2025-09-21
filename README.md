3b69c6f3bba343d3eb737369522cbbd9b09e7f66
Fang0428 <zhengjun.fang0428@gmail.com>
2025-09-21 20:38:53 +1000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fang0428.github.io
---
+++ src/utils/frontmatter.ts
@@ -0,0 +1,50 @@
+import getReadingTime from 'reading-time';
+import { toString } from 'mdast-util-to-string';
+import { visit } from 'unist-util-visit';
+import type { RehypePlugin, RemarkPlugin } from '@astrojs/markdown-remark';
+
+export const readingTimeRemarkPlugin: RemarkPlugin = () => {
+  return function (tree, file) {
+    const textOnPage = toString(tree);
+    const readingTime = Math.ceil(getReadingTime(textOnPage).minutes);
+
+    if (typeof file?.data?.astro?.frontmatter !== 'undefined') {
+      // Expose readingTime to frontmatter for templates that use it
+      file.data.astro.frontmatter.readingTime = readingTime;
+    }
+  };
+};
+
+export const responsiveTablesRehypePlugin: RehypePlugin = () => {
+  return function (tree) {
+    if (!tree.children) return;
+
+    for (let i = 0; i < tree.children.length; i++) {
+      const child = tree.children[i] as any;
+
+      if (child.type === 'element' && child.tagName === 'table') {
+        tree.children[i] = {
+          type: 'element',
+          tagName: 'div',
+          properties: { style: 'overflow:auto' },
+          children: [child],
+        } as any;
+        i++;
+      }
+    }
+  };
+};
+
+export const lazyImagesRehypePlugin: RehypePlugin = () => {
+  return function (tree) {
+    if (!tree.children) return;
+
+    visit(tree as any, 'element', function (node: any) {
+      if (node.tagName === 'img') {
+        node.properties = node.properties || {};
+        node.properties.loading = 'lazy';
+      }
+    });
+  };
+};
+
